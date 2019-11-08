42a1032e5df0968fafe2b2d829f76481812da9ef
greathd <2895284785@qq.com>
2019-11-08 23:50:42 +0800

    将原有项目拆分成为两个部分，分别为Server和Clinet。均基于SpringBoot2.2.1

---
+++ CloudClass-Clinet/src/main/java/com/bsbz/cloudclass/clinet/ClinetApplication.java
@@ -0,0 +1,13 @@
+package com.bsbz.cloudclass.clinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinetApplication.class, args);
+    }
+
+}
